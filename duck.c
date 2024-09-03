0c9d534a7f65e59c2ba0a6d76f9b0c1e7584b20b
Yuvraj Singh Chauhan <133221941+ysinghc@users.noreply.github.com>
2024-09-03 10:29:21 +0000

    talking to duck

--- duck.c
+++ duck.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
     struct tm tm = *localtime(&t);
 
     char filename[100];
-    sprintf(filename, "%02d-%02d-%d-%02d-%02d.md", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_hour, tm.tm_min);
+    sprintf(filename, "%d-%02d-%02d-%02d-%02d.md", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min);
 
     FILE *file = fopen(filename, "w");
 
@@ -20,16 +20,17 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    char template[] = "---\ntitle: %s\ncategories: General\n---\n\n";
-    fprintf(file, template, filename);
+    char template[] = "---\ntitle: %s\ncategory: %s\n---\n\n";
+    fprintf(file, template, filename, argv[1]);
 
-    for(int i = 1; i < argc; i++) {
+    for(int i = 2; i < argc; i++) {
         fprintf(file, "%s ", argv[i]);
     }
     fclose(file);
 
     char command[500];
-    sprintf(command, "mv %s ../_posts/%s", filename, filename);
+    //move the file to the _posts folder
+    sprintf(command, "mv %s ../_posts", filename);
     system(command);
 
     system("git add .");
