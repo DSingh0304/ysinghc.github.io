b3678aa89bcfaea08dadb2c0d36c52bc2b33645c
klarab <imv-in>
2024-09-03 17:04:33 +0530

    testing

--- setup.py
+++ setup.py
@@ -4,3 +4,4 @@ sbp.run(["sudo", "apt", "install", "git", "-y"])
 sbp.run(["git", "clone", "https://github.com/ysinghc/ysinghc.github.io.git"])
 sbp.run(["git", "config", "--global", "user.name", "\"klarab\""])
 sbp.run(["git", "config", "--global", "user.email", "\"imv-in\""])
+
